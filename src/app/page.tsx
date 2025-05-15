8e855e90a7f35d4d65d7622da2cfd4cae39b0c93
emmajphillips <emmjphillips@gmail.com>
2025-05-15 16:31:57 +0400

    Create initial app functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,69 @@
-import Image from "next/image";
+type Race = {
+  date: string;
+  raceName: string;
+  round: number;
+  season: string;
+  Sprint: object;
+};
 
-export default function Home() {
+const getCurrentSeasonRaces = async () => {
+  const response = await fetch('https://api.jolpi.ca/ergast/f1/current/');
+  const data = await response.json();
+
+  return data.MRData.RaceTable.Races;
+};
+
+export default async function Home() {
+  const races = await getCurrentSeasonRaces();
+
+  const now = Date.now();
+
+  const upcomingRaces = races?.filter(
+    (race: Race) => Date.parse(race.date) - now > 0
+  );
+  const nextRace = upcomingRaces[0];
+  const pastRaces = races?.slice(0, races.length - upcomingRaces.length);
+
+  const isRaceWeekend = () => {
+    return Date.parse(nextRace.date) - now < 604800000; // 7 days in milliseconds
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <span>Is there a race this weekend?</span>
+      <div>
+        {isRaceWeekend() ? (
+          <div>
+            Yes, It&apos;s the: {nextRace.raceName}
+            <div>
+              <p>Race weekend details:</p>
+              <p>{nextRace.date}</p>
+              <p>{nextRace.time}</p>
+              {nextRace.Sprint && <p>Psst...It&apos;s a sprint weekend</p>}
+            </div>
+          </div>
+        ) : (
+          <div>No, next race is the: {nextRace.raceName}</div>
+        )}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <p>Upcoming races</p>
+        {upcomingRaces.map((race: Race) => (
+          <div key={race.round + race.raceName + race.season}>
+            <p>Round {race.round}</p>
+            <p>{race.raceName}</p>
+            <p>{race.date}</p>
+            {race.Sprint && <p>Sprint weekend!</p>}
+          </div>
+        ))}
+        <p>Previous races this season:</p>
+        {pastRaces.map((race: Race) => (
+          <div key={race.round + race.raceName + race.season}>
+            <p>Round {race.round}</p>
+            <p>{race.raceName}</p>
+            <p>{race.date}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
